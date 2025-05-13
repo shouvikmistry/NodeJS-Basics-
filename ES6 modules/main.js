23b2780bf822c9df2768eee2439fdcba0b8a80ef
Shouvik <shouvikmistry24@gmail.com>
2025-05-13 19:53:21 +0530

    Default Export applied

--- ES6 modules/main.js
+++ ES6 modules/main.js
@@ -7,6 +7,10 @@ import http from "http";
 //Named Export imported
 import{a,b,c,d,e,f} from "./myModule.js";
 
+//Default Export imported
+// import obj from "./myModule.js";
+import anyname from "./myModule.js"; //default export can be exported in any name
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -18,6 +22,12 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+
   //name export value printed
   console.log(`Named export value of a : ${a}, b : ${b}, c : ${c}, d : ${d}, e : ${e}`);
+
+  //default export value printed
+  // console.log(obj);
+  console.log(anyname);
+  
 });
--- ES6 modules/myModule.js
+++ ES6 modules/myModule.js
@@ -5,3 +5,10 @@ export const c = 3
 export const d = 4
 export const e = 5
 export const f = 6
+
+//Default Export
+let obj; //object vaiable shouble initialized before exporting
+export default obj = {
+    x : 5,
+    y : 7
+}
