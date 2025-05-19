46b7224e0fad05b6a4e9cc5431ee2f0620b98326
Shouvik <shouvikmistry24@gmail.com>
2025-05-20 02:01:56 +0530

    Add main.js to set up Express server with basic routes and static file serving

---
+++ ExpressJS/main.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//app.get('path', handler(callback))
+app.get('/', (req, res) => {
+  res.send('Hello World!')//response to the request
+})
+//Other routes
+app.get('/home', (req, res) => {
+  res.send('Welcome to the home page')
+})
+app.get('/about', (req, res) => {
+  res.send('About this site')
+})
+app.get('/contact', (req, res) => {
+  res.send('Contact us')
+})
+app.get('/products', (req, res) => {
+  res.send('Products page')
+})
+//-------------------------------------------------------
+// Variables query using slug
+//Dynamic route
+app.get('/blog/:slug', (req, res) => {
+    //for url http://localhost:3000/blog/java%20anything?mode=dark
+    console.log(req.params);// will output the slug :java anything
+    console.log(req.query);// will output the query string { mode: 'dark' }
+  res.send(`Introduction to blog post ${req.params.slug}`)
+  //params is an object containing properties mapped to the named route “parameters” (i.e. :slug) its comes from the URL
+})
+//-------------------------------------------------------
+//static files
+app.use(express.static('public'))
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
