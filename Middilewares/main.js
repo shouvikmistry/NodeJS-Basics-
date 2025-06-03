e74f4d6f16f8f6effd198fdb270c4e84629123f6
Shouvik <shouvikmistry24@gmail.com>
2025-06-04 03:52:52 +0530

    Middilewares Adeed

--- Middilewares/main.js
+++ Middilewares/main.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,3 +16,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
